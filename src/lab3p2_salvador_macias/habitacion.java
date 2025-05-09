a87c6ec864a0bc2ea6dcdd7db83afce64c4834e5
papilon10 <salvigna17@gmail.com>
2025-05-09 13:59:48 -0600

    clase habitacion creada

--- src/lab3p2_salvador_macias/habitacion.java
+++ src/lab3p2_salvador_macias/habitacion.java
@@ -9,5 +9,29 @@ package lab3p2_salvador_macias;
  * @author Apple
  */
 public class habitacion {
-    
+
+    public int numero;
+    public TipoHabitacion tipo;
+    public boolean ocupada;
+    public cliente clienteActual;
+
+    public boolean estaDisponible() {
+
+        return false;
+
+    }
+
+    public void reservar(cliente cliente) {
+
+    }
+
+    public void liberar() {
+
+    }
+
+    @Override
+    public String toString() {
+        return "habitacion{" + "numero=" + numero + ", tipo=" + tipo + ", ocupada=" + ocupada + ", clienteActual=" + clienteActual + '}';
+    }
+
 }
