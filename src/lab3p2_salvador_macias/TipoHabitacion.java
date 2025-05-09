a2037177ae85df107f98326ea692866cd5e88e50
papilon10 <salvigna17@gmail.com>
2025-05-09 13:54:48 -0600

    enum tipo de habitacion creado

--- src/lab3p2_salvador_macias/TipoHabitacion.java
+++ src/lab3p2_salvador_macias/TipoHabitacion.java
@@ -9,5 +9,18 @@ package lab3p2_salvador_macias;
  * @author Apple
  */
 public enum TipoHabitacion {
-    
+    SIMPLE(50.),
+    DOBLE(80.0),
+    SUITE(150.0);
+
+    public double precio;
+
+    private TipoHabitacion(double precio) {
+        this.precio = precio;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
 }
