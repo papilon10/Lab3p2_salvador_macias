6ff4eca4b1fe60dc3c3011db595e6a03a58283f3
papilon10 <salvigna17@gmail.com>
2025-05-09 14:06:40 -0600

    clase reserva creada

--- src/lab3p2_salvador_macias/reserva.java
+++ src/lab3p2_salvador_macias/reserva.java
@@ -9,5 +9,23 @@ package lab3p2_salvador_macias;
  * @author Apple
  */
 public class reserva {
-    
+
+    public habitacion habitacion;
+    public int noches;
+    public double costo_total;
+
+    public reserva(habitacion habitacion, int noches) {
+        this.habitacion = habitacion;
+        this.noches = noches;
+    }
+
+    public double getCosto_total() {
+        return costo_total;
+    }
+
+    @Override
+    public String toString() {
+        return "reserva{" + "habitacion=" + habitacion + ", noches=" + noches + ", costo_total=" + costo_total + '}';
+    }
+
 }
